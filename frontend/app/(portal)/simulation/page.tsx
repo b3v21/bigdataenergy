77d604f0c65a84436b6ed217bc94b8593b5f7bdc
Levi <levijubb@gmail.com>
2023-08-23 17:18:24 +1000

    added sidebar

--- frontend/app/(portal)/simulation/page.tsx
+++ frontend/app/(portal)/simulation/page.tsx
@@ -3,21 +3,27 @@
 import Plot from 'react-plotly.js';
 
 import { data, layout } from './plot';
+import Sidebar from './components/sidebar';
 
 const Simulation = () => {
 	return (
-		<Plot
-			data={data}
-			layout={layout}
-			// config={{
-			// 	mapboxAccessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN
-			// }}
-			style={{
-				borderRadius: 'var(--radius)',
-				overflow: 'hidden',
-				minHeight: '800px'
-			}}
-		/>
+		<div className="flex flex-row gap-8">
+			<Sidebar />
+			<div className="flex-1">
+				<Plot
+					data={data}
+					layout={layout}
+					// config={{
+					// 	mapboxAccessToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN
+					// }}
+					style={{
+						borderRadius: 'var(--radius)',
+						overflow: 'hidden',
+						minHeight: '800px'
+					}}
+				/>
+			</div>
+		</div>
 	);
 };
 
