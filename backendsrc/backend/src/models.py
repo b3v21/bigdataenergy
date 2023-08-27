621a296b440a43640dcc02428afdcd906e5a73df
b3v21 <b3nn2121@gmail.com>
2023-08-27 17:53:08 +1000

    feat: models moved to model.py file

--- backendsrc/backend/src/models.py
+++ backendsrc/backend/src/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Station(models.Model):
+    station_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    lat = models.FloatField()
+    long = models.FloatField()
+
+    def __str__(self):
+        return self.headline
+    
+class Route(models.Model):
+    route_id = models.IntegerField(primary_key=True)
+    station_id = models.ForeignKey(Station, on_delete=models.CASCADE)
+    station_seq = models.IntegerField(default=1)
+    start = models.BinaryField()
+    stop = models.BinaryField()
+    
+class Timetable(models.Model):
+    station_id = models.ForeignKey(Station, on_delete=models.CASCADE)
+    route_id = models.ForeignKey(Route, on_delete=models.CASCADE)
+    arrival_times = models.CharField(max_length=255)
+
+all_stations = Station.objects.all()
