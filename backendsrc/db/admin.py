f94fe9fe847233eeca24e381cdf96938e4e9c61e
Harrison Bohl <bohlharrison357@gmail.com>
2023-08-30 14:45:32 +1000

    feat: initial admin skeleton

--- backendsrc/db/admin.py
+++ backendsrc/db/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from models import Timetable, Route, Station
 
-# Register your models here.
+
+class TimeTableAdmin(admin.ModelAdmin):
+    pass
+
+
+class RouteAdmin(admin.ModelAdmin):
+    pass
+
+
+class StationAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Timetable, TimeTableAdmin)
+admin.site.register(Timetable, RouteAdmin)
+admin.site.register(Timetable, StationAdmin)
