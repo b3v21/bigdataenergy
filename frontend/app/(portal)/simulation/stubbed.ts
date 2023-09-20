46d68f876199c5a7b906a3cd593201f1436b29d9
Josephine Davison <the.davisons.josie@gmail.com>
2023-09-20 14:30:51 +1000

    Update stubbed.ts
    feat: added callAPI function

--- frontend/app/(portal)/simulation/stubbed.ts
+++ frontend/app/(portal)/simulation/stubbed.ts
@@ -22,3 +22,25 @@ export const stubbedSimulation: Simulation = {
 		}
 	]
 };
+
+export const callAPI = async () => {
+	try {
+		const res = await fetch('http://127.0.0.1:8000/run_simulation/1/', {method: 'POST',headers: {'Accept': 'application/json','Content-Type': 'application/json', 'Access-Control-Allow-Origin':'*'}, body: `{
+      "env_start": 355,
+      "time_horizon": 30,
+      "itineraries": {
+        "0": [{
+          "route_id": "412-3136",
+          "start": "0",
+          "end": "1850"
+        }]
+      },
+      "snapshot_date": "2023-08-01"
+    }`});
+		const data = await res.json();
+		console.log(data);
+	} catch (err) {
+    console.log(err);
+	}
+};
+
